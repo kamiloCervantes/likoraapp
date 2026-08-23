0425467f90254a67846d82d9e5eff4649813c1e7
Camilo Cervantes <cacesa8931@gmail.com>
2026-08-23 07:58:40 -0500

    ajuste logs auth

--- backend/api/src/main.ts
+++ backend/api/src/main.ts
@@ -1,12 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api/v1');
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
+
   const port = process.env.PORT || 3000;
-  await app.listen(port);
-  console.log(`[Likora API] Servidor corriendo en el puerto ${port}`);
+  await app.listen(port, '0.0.0.0');
+  console.log(`[Likora API] Servidor corriendo en http://0.0.0.0:${port}/api/v1`);
 }
 bootstrap();
